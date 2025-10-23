9769745616caf06eacfc3fd54d3ee8c972c69203
手瓜一十雪 <nanaeonn@outlook.com>
2025-10-23 16:01:07 +0800

    Update test script and add SILK audio test file
    Added 'test.silk' audio file for testing. Updated test.js to include a SILK audio decoding test and a delay for attaching a debugger. Also updated VSCode settings to recognize additional C header files.

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
-const ffmpeg = require('../build/Release/ffmpegaddon');
-console.log('Test Video MP4:', ffmpeg.getVideoInfo('E:/NewDevelop/Ffmpeg/test/test.mp4'));
-console.log('Test Music MP3:', ffmpeg.getDuration('E:/NewDevelop/Ffmpeg/test/test.mp3'));
+console.log('Test Music MP3:', ffmpeg.getDuration('E:/NewDevelop/Ffmpeg/test/test.mp3'));console.log('等待40秒，可附加调试器...');
+setTimeout(() => {
+	console.log('Test Music SILK:', ffmpeg.decodeAudioToPCM('E:/NewDevelop/Ffmpeg/test/test.silk'));
+	// console.log('Test Music MP3:', ffmpeg.getDuration('E:/NewDevelop/Ffmpeg/test/test.mp3'));
+}, 15000);
