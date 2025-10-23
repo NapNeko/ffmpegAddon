547da130ea7998caf66d52cdc3af67e029dcffa5
手瓜一十雪 <nanaeonn@outlook.com>
2025-10-23 17:26:11 +0800

    Update patches and test for SILK codec integration
    Updated patch metadata to reflect new patch sequence and added a new patch for codec_desc.c. Modified test.js to enable duration testing for NTSilk files and commented out the SILK decode test. These changes support ongoing integration and testing of the SILK v3 codec.

--- test/test.js
+++ test/test.js
@@ -5,5 +5,5 @@ const mp4_test = path.join(__dirname, 'test.mp4');
 const ntsilk_test = path.join(__dirname, 'test.ntsilk');
 console.log('Test Video MP4:', ffmpeg.getVideoInfo(mp4_test));
 console.log('Test Music MP3:', ffmpeg.getDuration(mp3_test));
-// console.log('Test Music MP3:', ffmpeg.getDuration(ntsilk_test));
-console.log('Test Music SILK:', ffmpeg.decodeAudioToPCM(ntsilk_test));
+console.log('Test Music MP3:', ffmpeg.getDuration(ntsilk_test));
+//console.log('Test Music SILK:', ffmpeg.decodeAudioToPCM(ntsilk_test));
